c6fdaa1201054fb8bfb19ca219abca9b0a53cdd7
dadalton <ddalton94@hotmail.com>
2017-04-11 14:09:16 -0500

    ghost node implemented; code not functional

--- src/helmholtz.m
+++ src/helmholtz.m
@@ -7,35 +7,39 @@
 ax = -pi; ay = ax; %given domain limits, these form a rectangle
 bx = pi; by = bx;
 
+lambda = 1; %given value for lambda
+
 delta = 0.1; %step size, same for both x and y
 
 x = ax:delta:bx;  %discretizing the domain
-y = ay:delta:ax;
+y = ay:delta:by;
 
 gb = (bx-x).^2 .* cos((pi*x)/bx); %boundary conditions for y
 fb = x .* (bx - x).^2;
 
 F = sin(pi * (x - ax)/(bx - ax)) ... %applied force
-    * cos((pi/2)*(2*(y - ay)/(by - ay) + 1));
+    .* cos((pi/2)*(2*(y - ay)/(by - ay) + 1));
 
 %% gauss-seidel method
 
 u = zeros(length(x)); %initial values of u to be iterated over
+u(1,:) = gb;
+u(end,:) = fb;
+u(:,1) = gb(1) + (y-ay)/(by-ay) * (fb(1)-gb(1));
 
-for j = 1:length(y)
-    for i = 1:length(x)
-        uprev = u(i,j);
-        u(i,j) = (u(i+1,j) + u(i-1,j) + u(i,j+1) + u(i,j-1) ...
-            - delta^2 * F(i,j)) * (1/(4 - delta^2 * lambda));
-        epsilon = abs((u(i,j)-uprev)/u(i,j));
-        if epsilon < 0.01   %terminates iteration for less than 1% change
-            break
+epsilon = ones(length(x));
+while epsilon > 0.1
+    uprev = u;
+    for i = 2:length(y)-1
+        for j = 2:length(x)-1
+            u(j,i) = (u(j+1,i) + u(j-1,i) + u(j,i+1) + u(j,i-1) ...
+                - delta^2 * F(j));% * (1/(4 - delta^2 * lambda));
         end
+        u(i,end) = 2*u(i,end-1) + u(i+1,end) + u(i-1,end) - delta^2*F(i);
     end
+    u = u * 1/(4 - delta^2 * lambda); %applying constant denominator coefficient
+    epsilon = abs(max(max((u-uprev)./u)));
 end
-
-%u = u * 1/(4 - delta^2 * lambda); %applying constant denominator coefficient
-
 
 
 
