882910af40382407a95ff07eb3931b88f6075c38
dadalton <ddalton94@hotmail.com>
2017-04-12 13:14:33 -0500

    implemented SOR, also functional. code appears to be fully operational

--- src/helmholtz.m
+++ src/helmholtz.m
@@ -2,7 +2,7 @@
 %Scientific Computing - MECE 5397
 %Implementation of Helmholtz Equation in 2D - Semester Project
 %Project code AHc2-1
-
+clear all; clc;
 %% given values and boundary conditions
 ax = -pi; ay = ax; %given domain limits, these form a rectangle
 bx = pi; by = bx;
@@ -20,30 +20,63 @@ fb = x .* (bx - x).^2;
 F = sin(pi * (x - ax)/(bx - ax)) ... %applied force
     .* cos((pi/2)*(2*(y - ay)/(by - ay) + 1));
 
-%% gauss-seidel method
+%% Gauss-Seidel/Liebmann method
 
 u = zeros(length(x)); %initial values of u to be iterated over
-u(1,:) = gb;
-u(end,:) = fb;
-u(:,1) = gb(1) + (y-ay)/(by-ay) * (fb(1)-gb(1));
+u(1,:) = gb;          %boundary condition for y (bottom)
+u(end,:) = fb;        %boundary condition for y (top)
+u(:,1) = gb(1) + (y-ay)/(by-ay) * (fb(1)-gb(1)); %bc for x (left)
 
-iter = 0;
-epsilon = ones(length(x));
-while epsilon > 0.1
-    uprev = u;
+iter = 0;                   %used to count number of iterations
+epsilon = ones(length(x));  %calculating relative change per iteration
+while epsilon > 0.01
+    uprev = u;              %iteration reference
+    
+    %sweeping through columns and rows, iterating values
     for i = 2:length(y)-1
         for j = 2:length(x)-1
             u(j,i) = (u(j+1,i) + u(j-1,i) + u(j,i+1) + u(j,i-1) ...
                 - (delta^2) * F(j)) * (1/(4 - delta^2 * lambda));
         end
+        
+        %right side x bc is a Neumann condition (insulated)
         u(i,end) = (2*u(i,end-1) + u(i+1,end) + u(i-1,end) - (delta^2)*F(i)) ...
-            * (1/(4 - delta^2 * lambda));
+            * (1/(4 - delta^2 * lambda)); 
     end
-    epsilon = abs(max(max((u-uprev)./u)));
-    iter = iter + 1;
+    %the largest value change in the matrix will determine the epsilon
+    epsilon = abs(max(max((u-uprev)./u)));  
+    iter = iter + 1;                        %counting the iterations
 end
 
+%% Gauss-Seidel with Successive Overrelaxtion (SOR)
 
+SORlambda = 1.2;         %coefficient to expedite convergence
 
+SORu = zeros(length(x)); %initial values of u to be iterated over
+SORu(1,:) = gb;          %boundary condition for y (bottom)
+SORu(end,:) = fb;        %boundary condition for y (top)
+SORu(:,1) = gb(1) + (y-ay)/(by-ay) * (fb(1)-gb(1)); %bc for x (left)
 
-
+SORiter = 0;                   %used to count number of iterations
+SORepsilon = ones(length(x));  %calculating relative change per iteration
+while SORepsilon > 0.01
+    SORuprev = SORu;              %iteration reference
+    
+    %sweeping through columns and rows, iterating values
+    for i = 2:length(y)-1
+        for j = 2:length(x)-1
+            SORu(j,i) = (SORu(j+1,i) + SORu(j-1,i) + SORu(j,i+1) + SORu(j,i-1) ...
+                - (delta^2) * F(j)) * (1/(4 - delta^2 * lambda));
+            SORu(j,i) = SORlambda * SORu(j,i) + (1-SORlambda) * SORuprev(j,i);
+        end
+        
+        %right side x bc is a Neumann condition (insulated)
+        SORu(i,end) = (2*SORu(i,end-1) + SORu(i+1,end) + SORu(i-1,end) - (delta^2)*F(i)) ...
+            * (1/(4 - delta^2 * lambda)); 
+        SORu(i,end) = SORlambda * SORu(i,end) + (1-SORlambda) * SORuprev(i,end);
+        
+    end
+    %the largest value change in the matrix will determine the epsilon
+    SORepsilon = abs(max(max((SORu-SORuprev)./SORu)));  
+    SORiter = SORiter + 1;                        %counting the iterations
+end
